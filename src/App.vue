1bb156714308608f6c4e1a2ad686effa29cd5d14
GabrielLima2803 <gabriellima2803@gmail.com>
2023-06-21 16:31:15 -0300

    Co-authored-by: Theodoro Gamer <TheodoroObenaus@users.noreply.github.com>
    Botar modal

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup> 
-  import { ref } from 'vue'
+  // import { ref } from 'vue'
 
-  const alternativas = ref([
-    {
-    id: 1,
-    nome: 'Camiseta',
+  // const alternativas = ref([
+  //   {
+  //   id: 1,
+  //   nome: 'Camiseta',
     
-    }
-  ])
+  //   }
+  // ])
 </script>
 
 <template>
@@ -19,7 +19,7 @@
       
     </div>
     <div class="materia">
-      <h2>MathQuiz</h2>
+      <h2 class="fodao">MathQuiz</h2>
     </div>
   </header>
 
@@ -29,7 +29,7 @@
     <div class="container">
       <div class="Questao">
         <div class="pergunta">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quia nesciunt non iusto, dolorum aliquam autem quod ipsa excepturi dolorem modi deleniti officia nam quas illo sequi dicta voluptatibus iste? barigadam Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium ducimus tempore molestias dolorem delectus fugit illum provident illo earum quas, corrupti non tenetur labore in? At saepe possimus inventore sed? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate optio voluptatibus soluta voluptatem pariatur ducimus ipsum modi quam voluptates magni quod nulla perferendis, ut culpa ex, explicabo dolore illo odio.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quia nesciunt non iusto, dolorum aliquam autem quod ipsa excepturi dolorem modi deleniti officia nam quas illo sequi dicta voluptatibus iste? barigaaaadam Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium ducimus tempore molestias dolorem delectus fugit illum provident illo earum quas, corrupti non tenetur labore in? At saepe possimus inventore sed? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate optio voluptatibus soluta voluptatem pariatur ducimus ipsum modi quam voluptates magni quod nulla perferendis, ut culpa ex, explicabo dolore illo odio.
         </div>
         
         <div class="img">
@@ -39,10 +39,10 @@
 
       <div class="alternativas">
 
-        <button type="button" class="" >1</button>
-        <button type="button" class="" >2</button>
-        <button type="button" class="" >3</button>
-        <button type="button" class="" >4</button>
+        <button type="button" class="botaoUm" >1</button>
+        <button type="button" class="botaoDois" >2</button> 
+        <button type="button" class="botaoDois " >3</button>
+        <button type="button" class="botaoUm" >4</button>
         
       </div>
 
@@ -60,7 +60,7 @@
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-right: 10px;
+  margin-right: 30px;
 }
 
 .Questao{
@@ -77,10 +77,12 @@
   padding: 20px 20px;
   border: 2px solid gray;
   border-radius: 10px 10px 10px 10px;
-  width: 70%;
+  width: 50%;
   background-color: rgb(26, 25, 25);
   font-family: 'Comic Neue', cursive;
-
+  font-size: 23px;
+  text-align: justify;
+  margin-right: 25px;
 }
 .NQuestao{
   display: flex;
@@ -88,8 +90,9 @@
   justify-content: center;
   color: white;
   text-align: center;
-  background: rgb(51, 13, 82);
+  background: rgb(221, 38, 38);
   margin: 10px;
+  margin-left: 20px;
   border-radius: 50%;
   width: 50px;
   height: 50px;
@@ -97,5 +100,34 @@
 }
 .numero{
   margin: 0;
+}
+
+.botaoUm {
+  border: none;
+  color: white;
+  background: rgb(163, 17, 17);
+  width: 350px;
+  height: 100px;
+  margin-bottom: 25px;
+}
+.botaoDois{
+  border: none;
+  color: white;
+  background: rgb(221, 38, 38);
+  width: 350px;
+  height: 100px;
+  margin-bottom: 25px;
+}
+.alternativas{
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  grid-template-columns: 375px 375px;
+  margin-top: 10%;
+}
+
+.fodao {
+  font-size: 60px;
+  font-family: 'Comic Neue', cursive;
 }
 </style>
